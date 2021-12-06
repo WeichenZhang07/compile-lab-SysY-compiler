1c43e4e594132e0bac9b3ef5483e05d4cd181765
Weichen Zhang <zhangweichen1873@buaa.edu.cn>
2021-12-06 15:38:05 +0800

    短路求值未完成 第一次push

--- tests/Test1.c
+++ tests/Test1.c
@@ -1,5 +1,14 @@
-int a[1][1][10] = {{{1,2,3}}};
-
 int main() {
-
+    int a, b, c = 1, d;
+    int result;
+    a = 5;
+    b = 5;
+    d = -2;
+    result = 2;
+    if (a + b == 9 || a - b == 0 && result != 4)
+        result = result + 3;
+    else if (c + d != -1 || (result + 1) % 2 == 1)
+        result = result + 4;
+    putint(result);
+    return 0;
 }
